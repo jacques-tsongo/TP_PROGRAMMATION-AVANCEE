8056362ba2b200ff2bccb2f958a2036dcb51549c
jacques-tsongo <kaserekatsongojacques2023@gmail.com>
2026-02-12 17:00:56 +0200

    premier commit

---
+++ Form6.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TRAVAIL_PROGRAMMATION_AVANCEE
+{
+    public partial class reportForm : Form
+    {
+        public reportForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
